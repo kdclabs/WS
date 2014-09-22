27a34aa0fdcd410051fead653a45c341c97053a9
Vachan Kudmule <vachan@kdc.in>
2014-09-22 10:22:58 +0530

    Update ws_dash.php
    Code Clean up

--- ws_dash.php
+++ ws_dash.php
@@ -7,14 +7,14 @@ Version: 1.0
 */ 
 
 // changing the login page URL
-function ws_login_logo() {
+function ws_login_logo() { ?>
     <style type="text/css">
         body.login div#login h1 a {
             background-image: url('https://ws.ai/img/wp/ws-login-logo.png?ver=20140922');
             background-image: none, url('https://ws.ai/img/wp/ws-login-logo.svg?ver=20140922');
         }
     </style>
-}
+<?php }
 add_action( 'login_enqueue_scripts', 'ws_login_logo' );
 
 // changing the login page URL
@@ -34,3 +34,5 @@ function ws_login_message() {
     return 'For assistance please call WhiteShadows helpline<br/><strong>1800 200 2442</strong> <em>{ext# 14}</em>';
 }
 add_filter( 'login_message', 'ws_login_message' );
+
+?>
